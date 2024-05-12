8ba87405e36f2839120d698e3f801804a3b401a8
grigbor <kolanchik796@gmail.com>
2024-05-12 20:17:03 +0200

    create second table

--- ashab/goods/admin.py
+++ ashab/goods/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from goods.models import Categories
+from goods.models import Categories, Products
 
 admin.site.register(Categories)
+admin.site.register(Products)
