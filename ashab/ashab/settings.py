84ab6cfc49b0e7baa1aa111e4c02132c001ba635
grigbor <kolanchik796@gmail.com>
2024-08-04 12:37:35 +0200

    qw

--- ashab/ashab/settings.py
+++ ashab/ashab/settings.py
@@ -70,10 +70,14 @@ WSGI_APPLICATION = 'ashab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER':'home',
+        'PASSWORD':'BatMan2012',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
-}
+} 
 
 
 # Password validation
